cf02e619aef87c2081b0955614979f4f393035ad
Dij1022 <90654140+Divija1967@users.noreply.github.com>
2021-11-23 13:04:58 -0500

    Added LA2Q1
    Testing and sorting methods first commit

---
+++ ViduniDivijaTestingSortingMethods.java
@@ -0,0 +1,72 @@
+package LA2Q1;
+
+public class ViduniDivijaTestingSortingMethods {
+    public static void header(){
+        System.out.println("*******************************************************\n" +
+                "Names: Divija Bhargava and Viduni Weihena Epa.\n" +
+                "Student Numbers: 251159008 and 251152022\n" +
+                "Goal of this project: Brief description of the project.\n" +
+                "*******************************************************");
+    }
+    public static void footer(){
+        System.out.println("***************************************\n" +
+                "This is timeOfDay on theDate.\n" +
+                "Completion of Lab Assignment 2 is successful!\n" +
+                "Good bye! Divija Bhargava and Viduni Weihena Epa \n" +
+                "***************************************");
+    }
+    public static <T extends Comparable <? super T>>long selectionSort (T [] a){
+        return 0;
+    }
+
+    public static < T extends Comparable <? super T >> long bubbleSort(T[] a){
+        return 0;
+    }
+
+    public static < T extends Comparable <? super T >> long insertionSort(T[] a){
+        return 0;
+    }
+
+    public static <T extends Comparable<? super T>> long mergeSort(T[] S){
+        return 0;
+    } //[you can tweak the
+    //recursive merge-sort code given in the Unit 4 – companion document]
+
+    public static <T extends Comparable<? super T>> long quickSort(T[] s, int a, int b){
+        return 0;
+    }//[you can
+    //implement the in-place quick-sort algorithm given in the lecture handout]
+
+    public static long bucketSort(Integer[] a, int first, int last, int maxDigits) {
+        return 0;
+    }
+
+    // driver method
+    public static void main(String[] args) {
+//        Call your header method.
+        header();
+//        Declare an Integer type array of a variable size sz which you can present to 50000.
+
+//        Create a backup Integer type array with the same size.
+
+//        Populate the first array with random values from 1 to sz, by using Math.random()
+//        method.
+
+//        Copy the content of the first array to the backup array (You can use
+//                System.arraycopy() method).
+
+//        Convert the first array to an ArrayList and then sort it using Collections’ sort
+//        method. Check the time and print it on the screen (see the sample output).
+
+//        Now swap the first array with the backup array (you need to do this to make sure
+//        that you are sorting the same unsorted array every time)
+
+//        Call the selectionSort() method by passing the first array, and print the time.
+
+//        Repeat steps vi) and vii) for each call, and print the time (see the sample output)
+
+//        Call your footer method.
+        footer();
+
+    }
+}
